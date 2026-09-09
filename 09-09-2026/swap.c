b12220e740a2676ecc71992c5eb4315a23bf1745
Madhur <ee26btech11208@iith.ac.in>
2026-09-09 15:22:39 +0530

    commit

--- 09-09-2026/swap.c
+++ 09-09-2026/swap.c
@@ -1,8 +1,14 @@
-#include <stdio.h>
-int main(){
+#include <stdio.h>// Madhur 09-09-2026function to swap 2 letters
+void swap(char *a){
+	char temp='i';
+	temp=a[0];
+	a[0]=a[1];
+	a[1]=temp;
+}
+int main(){//taking input
 	char a[43];
-	printf("Enter word\n")
-	scanf("%42s",a);
+	printf("Enter word\n");
+	scanf("%[^\n]",a);
 	char temp='l';
 	temp=a[0];
 	a[0]=a[1];
