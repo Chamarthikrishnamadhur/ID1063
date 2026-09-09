0a10479cdf5c79091663ea6e2e1c9453f7eb0e59
Madhur <ee26btech11208@iith.ac.in>
2026-09-09 15:00:17 +0530

    commit

--- 09-09-2026/occur.c
+++ 09-09-2026/occur.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 int main(){
-	char a[43];
+	char a[43],b[4];
 	printf("Enter word:");
-	scanf("%42s",a)
+	scanf("%42s",a);
+	printf("\n Enter char");
+	scanf("%2s",b);
+	printf("%c\n",b[0]);
+	int k=0;
+	for(int i=0;i<43;i++){
+		if(a[i]==b[0]){
+			k+=1;
+		}
+	}
+	printf("%d\n",k);
 }
