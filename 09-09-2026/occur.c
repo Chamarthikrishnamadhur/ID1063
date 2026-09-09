049b68b5b911b5c8dc6db8550b5ca1885a3499a9
Madhur <ee26btech11208@iith.ac.in>
2026-09-09 15:15:32 +0530

    commit

--- 09-09-2026/occur.c
+++ 09-09-2026/occur.c
@@ -1,16 +1,21 @@
+//code by madhur 09-09-2026
 #include<stdio.h>
 int main(){
+	//storing in artrays
 	char a[43],b[4];
+// Taking input for word and characxter
 	printf("Enter word:");
 	scanf("%42s",a);
 	printf("\n Enter char");
 	scanf("%2s",b);
 	printf("%c\n",b[0]);
+	//counting occurences 
 	int k=0;
 	for(int i=0;i<43;i++){
 		if(a[i]==b[0]){
 			k+=1;
 		}
 	}
-	printf("%d\n",k);
+	//printinbg occurences
+	printf("Occurences :%d\n",k);
 }
