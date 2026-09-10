b31cbbc139b093d9cfa01ebd36ddbff338097041
Madhur <ee26btech11208@iith.ac.in>
2026-09-10 17:10:11 +0530

    commit

--- 10-09-2026/raph.c
+++ 10-09-2026/raph.c
@@ -8,7 +8,7 @@ int main(){
 	double x=1;
 	//Newton raphson iteration
 	for (int i=0;i<50;i++){
-		//printf("%lf\n",x);
+		printf("%lf\n",x);
 		x=raph(x);
 	}
 	printf("Newton raphson gave %lf\n",x);
