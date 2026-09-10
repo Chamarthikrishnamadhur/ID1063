aad70503df85798d44854853126c1918317bcf4d
Madhur <ee26btech11208@iith.ac.in>
2026-09-10 16:46:29 +0530

    commit

--- 10-09-2026/raph.c
+++ 10-09-2026/raph.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 #include<math.h>
+//Function to give raphson
 double raph(double x){
 	return (x-((exp(x)-2)/(exp(x))));
 }
 int main(){
 	double x=1;
-	for (int i=0;i<20;i++){
-		printf("%lf\n",x);
+	//Newton raphson iteration
+	for (int i=0;i<50;i++){
+		//printf("%lf\n",x);
 		x=raph(x);
 	}
-	printf("Newton raphson gave %lf\n",x)
+	printf("Newton raphson gave %lf\n",x);
 }
