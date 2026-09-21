a57d78535be66bf7b2686fa67197e8686dc88c93
Madhur <ee26btech11208@iith.ac.in>
2026-09-21 15:34:20 +0530

    commit

---
+++ 21-09-26/diff.py
@@ -0,0 +1,16 @@
+import subprocess
+import shlex
+import matplotlib.pyplot as plt
+import numpy as np
+x=np.linspace(-1,0,1000)
+x1-=np.linspace(0,1,1000)
+y=np.sin(2*x)
+a=0
+b=2
+x2=np.linspace(-1,1)
+y2=(2*x2)
+y1=a+b*x1
+plt.plot(x,y)
+plt.plot(x1,y1)
+plt.plot(x2,y2)
+
