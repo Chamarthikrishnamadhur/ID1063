6f8b43d966c49f7fafee0351bfc5baeb975a89cc
Madhur <ee26btech11208@iith.ac.in>
2026-09-21 15:37:35 +0530

    commit

--- 21-09-26/diff.py
+++ 21-09-26/diff.py
@@ -3,14 +3,16 @@ import shlex
 import matplotlib.pyplot as plt
 import numpy as np
 x=np.linspace(-1,0,1000)
-x1-=np.linspace(0,1,1000)
+x1=np.linspace(0,1,1000)
 y=np.sin(2*x)
 a=0
 b=2
-x2=np.linspace(-1,1)
+x2=np.linspace(-0.25,0.25)
 y2=(2*x2)
 y1=a+b*x1
 plt.plot(x,y)
 plt.plot(x1,y1)
+plt.plot(0,0,marker='o')
 plt.plot(x2,y2)
-
+plt.savefig("code.pdf")
+subprocess.run(shlex.split("termux-open code.pdf"))
