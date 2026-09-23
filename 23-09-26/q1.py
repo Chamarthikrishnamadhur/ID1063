d7fdb87438208bb7f000352bb5d5625299b10a76
Madhur <ee26btech11208@iith.ac.in>
2026-09-23 15:04:26 +0530

    commit

--- 23-09-26/q1.py
+++ 23-09-26/q1.py
@@ -1,8 +1,13 @@
+#code by madhur
+#on 23-09-26
+#importing function
 import numpy as np
+#input
 A=eval(input("Enter the list "))
 a=np.array(A)
 a=a*a
-print(a)
+#print(a)
 s=np.mean(a)
 s=np.sqrt(s)
+#we took squraes and mean and put root
 print(round(s,2))
